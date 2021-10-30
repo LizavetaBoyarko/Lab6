2c4a9669749b2804b8a26fa7e3aba0ade6fc91f7
Елизавета Боярко <LizOK_boarko@mail.ru>
2021-10-31 00:41:05 +0300

    Добавьте файлы проекта.

---
+++ Задание 6.2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение: ");
+            string startString = Console.ReadLine();
+            int len = startString.Length;
+            int max = 0;
+            int count = 0;
+            int ind = 0;
+            for (int i = 0; i < len; i++)
+            {
+                Console.WriteLine("Введите предложение:");
+
+                string startString = Console.ReadLine();
+                startString = startString.ToLower();
+                startString = startString.Replace(" ", "");
+
+                char[] tmp = startString.ToCharArray();
+                Array.Reverse(tmp);
+
+                string reverseString = String.Concat<char>(tmp);
+
+                int flag = 1;
+
+                for (int i = 0; i < startString.Length; i++)
+                {
+                    if (startString[i] == reverseString[i])
+                    {
+                        flag = 1;
+                        continue;
+                    }
+                    else
+                    {
+                        flag = 2;
+                        break;
+                    }
+
+                }
+
+                if (flag == 1)
+                {
+                    Console.WriteLine("Да, предложение палиндром");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Нет, предложение не палиндром");
+                    Console.ReadKey();
+                }
+
+                Console.WriteLine("End");
+                Console.ReadKey();
+            }
+    }
+}
---
+++ Задание 6/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение: ");
+            string startString = Console.ReadLine();
+            int len = startString.Length;
+            int max = 0;
+            int count = 0;
+            int ind = 0;
+            for (int i = 0; i < len; i++)
+            {
+                if (startString[i] != ' ')
+                {
+                    count += 1;
+                }
+                else
+                {
+                    if (count > max)
+                    {
+                        count = max;
+                        ind = i - count;
+                    }
+                    count = 0;
+                }
+                if (count > max)
+                {
+                    max = count;
+                    ind = i - count + 1;
+                }
+
+            }
+
+            Console.WriteLine(startString.Substring(ind, max));
+            Console.ReadKey();
+        }
+    }
+}
